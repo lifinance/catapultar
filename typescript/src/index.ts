871a3b19418ea899cf852f6a19605a8db44016f2
Leonardo Cascianelli <leo@li.finance>
2026-01-27 12:18:29 +0100

    feat: barrel export

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,12 +1,15 @@
 export { CatapultarAccount } from "./catapultar/account";
 export { CatapultarTx, MetaCatapultarTx } from "./catapultar/catapultar";
+export { ConstrainedAssetTransaction } from "./transaction/constrainedtransaction";
+export { BaseTransaction } from "./transaction/transaction";
+
+export { ExecutionMode, AccountPublicKeyType } from "./types/types";
 
 export type {
-  ExecutionMode,
-  AccountPublicKeyType,
+  AccountPublicVar,
   Call,
   Calls,
+  Allowance,
+  Outcome,
   WebAuthnSignature,
 } from "./types/types";
-
-export type { BaseTransaction } from "./transaction/transaction";
