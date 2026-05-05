4ab0341aee1724521265669b5eca6d89dd4dc4ee
Alexander <git@lindgren.xyz>
2026-05-05 12:27:17 +0200

    Update solidity pragma version

--- solidity/src/libs/IntentExecutor.sol
+++ solidity/src/libs/IntentExecutor.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.17;
+pragma solidity ^0.8.30;
 
 import { LibBit } from "solady/src/utils/LibBit.sol";
 import { ReentrancyGuard } from "solady/src/utils/ReentrancyGuard.sol";
--- solidity/src/libs/LibClone.tron.sol
+++ solidity/src/libs/LibClone.tron.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.4;
+pragma solidity ^0.8.30;
 
 import { LibClone } from "solady/src/utils/LibClone.sol";
 
